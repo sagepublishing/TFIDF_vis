ea52fb0217c09d94fed0190ebfd07a1408342811
A D <ad48@users.noreply.github.com>
2018-04-04 23:24:58 +1200

    Fix alpha calculation; allow averaging cites over select years
    This should mean the visualisation doesn't suffer from misleading colouring where we have a lot of recent data inside a cluster.  Update to config.py, too, to allow selection of the years to average over.

--- config.py
+++ config.py
@@ -3,26 +3,29 @@ import os
 
 class Config():
     # Name the dataset
-    set_name = 'Cardiology'
+    set_name = 'Continuity in education'
     # Enter your own email address
     myemail = 'adam.day@sagepub.co.uk'
     # Journal of interest (must match the WoS name for the journal)
-    joi = 'BRAIN' 
+    joi = None #'BRAIN' 
     # which years' data will we use?  Edit to shrink your data to a smaller size for testing
-    years = ['2012','2013', '2014','2015','2016','2017'] # 2012-2017 available
-
-    # SETTINGS
+    years = ['2011','2012','2013', '2014','2015','2016','2017'] 
+    
+        # SETTINGS
     # sampling - take every nth article from the dataset.  If you don't want to sample, set equal to 1
     every_nth = 1
     # kmeans
-    n_clusters = 32
+    n_clusters = 20
     
     # tsne settings.  Set all to None for defaults
-    n_iter = 1500
+    n_iter = 1000
     n_iter_without_progress = 50
-    perplexity = 400 # recommendation is 5-7% of dataset size.
-    learning_rate = 150
+    perplexity = 20 # recommendation is 5-7% of dataset size.
+    learning_rate = 50
 
+    # Visualisation settings
+    # when choosing colours for clusters, which years of data would you like to average over? (better to avoid recent years since new articles have no citations)
+    av_years = [2014, 2015] # set to 'years' if unsure!
     
     # GLOBAL VARIABLES - probably won't need to edit these
     wos_word_data_p = 'data/wos_word_data.csv'    
